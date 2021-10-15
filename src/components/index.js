a0783728487213401d34c571633954cf584eb6b1
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-10-15 23:03:17 +0200

    feat: add basic routing and project architecture

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as NavBar } from "./Navbar/NavBar";
+export { default as Footer } from "./Footer/Footer";
+export { default as Banner } from "./Banner/Banner";
+export { default as PresentationCard } from "./Cards/PresentationCard/PresentationCard";
+export { default as UserAccountCard } from "./Cards/UserAccountCard/UserAccountCard";
---
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserProfilePage() {
+  return (
+    <div>
+      User profile page
+    </div>
+  )
+}
